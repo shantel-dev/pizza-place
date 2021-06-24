e6a58390d5d96a473a00824766fc06fbc919a212
shantel.kemunto@student.moringaschool.com <kemuntoshantel0@gmail.com>
2021-06-24 11:22:57 +0300

    Enable user to get the prices by inserting the type of crust size of pizza and amount of pizzas

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function getCostOfSize() {
+    var chosenSize = document.getElementById("size").value;
+    return parseInt(chosenSize);
+}
+
+function getCostOfCrust() {
+    var chosenCrust = document.getElementById("crust").value;
+    return parseInt(chosenCrust);
+}
+
+function getAmount() {
+    var chosenNumber = document.getElementById("number").value;
+    return parseInt(chosenNumber);
+}
